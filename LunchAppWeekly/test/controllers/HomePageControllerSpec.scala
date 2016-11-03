a61ba548ffc4472c3e1963103a87bcf1c322558f
sara202 <sara.202@hotmail.co.uk>
2016-11-03 12:10:22 +0000

    End of session 4.

--- LunchAppWeekly/test/controllers/HomePageControllerSpec.scala
+++ LunchAppWeekly/test/controllers/HomePageControllerSpec.scala
@@ -25,12 +25,34 @@ class HomePageControllerSpec  extends PlaySpec with OneAppPerSuite{
 
   "HomePageController"  should {
 
-    "say morning " when {
+    "say morning " ignore {
       val result = controller.land()(FakeRequest(GET, "foo"))
       status(result) mustBe OK
-      contentAsString(result) must include ("Lunch?")
-      contentAsString(result) must include ("Morning")
+      contentAsString(result) must include("Lunch?")
+      contentAsString(result) must include("Morning")
     }
+
+    "say afternoon " in  {
+      val result = controller.land()(FakeRequest(GET, "foo"))
+      status(result) mustBe OK
+      contentAsString(result) must include("Lunch?")
+      contentAsString(result) must include("Afternoon")
+    }
+
+    "have some content " ignore {
+      val result = controller.land()(FakeRequest(GET, "foo"))
+      status(result) mustBe OK
+      contentAsString(result) must include ("Morning want to order Lunch?")
+    }
+
+    "Say hello" when {
+      "I go to the landing page" in {
+        val result = route(app, FakeRequest(GET, "/landing"))
+        result.map(contentAsString(_)).get must include ("want to order Lunch?")
+      }
+    }
+
+
 
     "not return 404" when {
       "I go to the route /landing" in {
@@ -39,17 +61,6 @@ class HomePageControllerSpec  extends PlaySpec with OneAppPerSuite{
       }
     }
   }
-
-
-  "render the correct page with the expected text " when {
-    "I go to the homepage" in {
-      val result = controller.landing()(FakeRequest(GET, "/landing"))
-      status(result) mustBe OK
-      contentAsString(result) must include("Hello")
-
-    }
-  }
-
 
 
 
